cf8c5a0804ca6ad6b9ec2f7038a62ec3d78a610b
rladmstj <pingdoll3110@naver.com>
2024-07-15 17:56:39 +0900

    저금하기

---
+++ src/ItemDetail.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import './ItemDetail.css';
+
+const ItemDetail = () => {
+  const { itemId } = useParams(); // URL에서 itemId 파라미터를 가져옵니다
+  const [item, setItem] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchItem = async () => {
+      try {
+        const response = await axios.get(`http://43.200.215.241:2000/items/${itemId}`);
+        if (response.data.status === 'success') {
+          setItem(response.data.data);
+        } else {
+          setError(response.data.message);
+        }
+      } catch (error) {
+        setError('Error fetching item details');
+      }
+    };
+
+    fetchItem();
+  }, [itemId]);
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (!item) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="item-detail-container">
+      <div className="item-detail">
+        <img src={item.item_image_url} alt={item.name} className="item-image" />
+        <div className="item-info">
+          <h1>{item.name}</h1>
+          <p>Price: ${item.price.toLocaleString()}</p>
+          <p>{item.description1}</p>
+          <button className="buy-now-button">Buy Now</button>
+          <button className="buy-now-button">장바구니</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ItemDetail;
